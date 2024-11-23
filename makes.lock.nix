85005732b2e268e33fa9caa898f7f18e7e45db41
Daniel Betancur <125151874+dbetancurdev@users.noreply.github.com>
2024-11-23 06:44:36 -0500

    Update makes.lock.nix

--- makes.lock.nix
+++ makes.lock.nix
@@ -1,6 +1,6 @@
 {
-  makesSrc = builtins.fetchGit {
-    url = "https://github.com/fluidattacks/makes";
-    ref = "refs/heads/main";
+  makesSrc = builtins.fetchTarball {
+    sha256 = "0m4yyx3lznilzh88m42nh34s9apsqb7cl6rsixbgwklq5rxnprgg"; # Tarball sha256
+    url = "https://api.github.com/repos/fluidattacks/makes/tarball/1cde20d728032f9ab3bbd37e80a5531db6ff1b93";
   };
 }
